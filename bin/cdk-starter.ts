4c8e382df90c63a55d87f8f041401e0752f51735
Hasan <dev.hahuaz@gmail.com>
2023-01-11 17:51:44 +0300

    update node runtime. update dependencies. add scrapeItems functiom.

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,8 +3,8 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { BdoUpdate } from '../infrastructure/bdo-update';
 
-// to not pay secret manager cost, work with .env file. We won't be able to use CI/CD and only deployment will be available on current machine.
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// to not pay secret manager cost, work with .env file. We won't be able to use CI/CD and only deployment will be available on the current machine.
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = new cdk.App();
---
+++ lambda/scrape-items.ts
@@ -0,0 +1,42 @@
+const { COOKIE_REQUEST_TOKEN, COOKIE_TRADE_SESSION, BDOY_REQUEST_TOKEN } =
+  process.env;
+
+export async function main() {
+  console.log(COOKIE_REQUEST_TOKEN);
+  const data = await fetch(
+    'https://trade.tr.playblackdesert.com/Home/GetWorldMarketList',
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        origin: 'https://trade.tr.playblackdesert.com',
+        pragma: 'no-cache',
+        referer: 'https://trade.tr.playblackdesert.com/Home/list/hot',
+        'sec-ch-ua':
+          '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+        cookie: `__RequestVerificationToken=${COOKIE_REQUEST_TOKEN}; TradeAuth_Session=${COOKIE_TRADE_SESSION}; lang=en-US;`,
+      },
+      // use URLSearchParams for form-urlencoded content type
+      body: new URLSearchParams({
+        __RequestVerificationToken: BDOY_REQUEST_TOKEN as string,
+        mainCategory: '1',
+        subCategory: '2',
+      }),
+    }
+  );
+
+  const json = await data.json();
+  console.log(json);
+}
