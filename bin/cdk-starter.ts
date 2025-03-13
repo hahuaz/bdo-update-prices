7fdecaa0585c89cf38837349963902966ddffe2d
Hasan <dev.hahuaz@gmail.com>
2025-03-13 12:26:41 +0300

    chore: use env variables instead of cdk context

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { BdoUpdate } from '../infrastructure/bdo-update';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { BdoUpdate } from "../infrastructure/bdo-update";
 
 // to not pay secret manager cost, work with .env file. We won't be able to use CI/CD and only deployment will be available on the current machine.
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-const app = new cdk.App();
-const BRANCH = app.node.tryGetContext('BRANCH');
-const { APP_NAME, ACCOUNT, APP_REGION } = app.node.tryGetContext(BRANCH);
+const { APP_NAME, STAGE, AWS_ACCOUNT, AWS_REGION } = process.env;
+if (!APP_NAME || !STAGE || !AWS_ACCOUNT || !AWS_REGION) {
+  throw new Error("Missing required environment variables");
+}
 
-new BdoUpdate(app, `${BRANCH}-${APP_NAME}`, {
-  env: { account: ACCOUNT, region: APP_REGION },
+const app = new cdk.App();
+new BdoUpdate(app, `${STAGE}-${APP_NAME}`, {
+  env: {
+    region: AWS_REGION,
+    account: AWS_ACCOUNT,
+  },
 });
